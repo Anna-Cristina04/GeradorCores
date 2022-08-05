9f813a90a3ce83cd8ccd0b9fa428eb2aab894ec7
ANNA CRISTINA KOERICH <anna_koerich@estudante.sc.senai.br>
2022-08-04 21:48:50 -0300

    GeradorCores

---
+++ indexPadrao.js
@@ -0,0 +1,9 @@
+const hexCor = ["green","black","blue","red","purple","white","yellow"];
+
+
+function mudarCor() {
+        document.querySelector("h1").textContent = "Cor: "
+        cor = hexCor[Math.floor(Math.random() * hexCor.length)]
+        document.querySelector("body").style.backgroundColor =  cor;
+        document.querySelector('h1').textContent += cor;
+}
